3aa0b6ea01932750c9eda886b6523bb5607c5798
Logan Wood <lwood3499@gmail.com>
2020-11-23 12:39:40 -0600

    creating a deal function with multiple return values

--- cards/deck.go
+++ cards/deck.go
@@ -6,10 +6,27 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
-// (d deck) is a 'receive' of a function
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// (d deck) is a 'receiver' of a function
 // this establishes that any variable of type 'deck' gets access to the print method
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// function takes 2 specific parameters, type deck and type int, returns to specific values of type deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,9 @@
 package main
 
 func main() {
-	// NOTE: Array is fixed length, Slice can have length changed
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Diamonds") // does not mutate original slice, but returns a new slice
+	cards := newDeck()
 
-	cards.print()
-}
-
-// function that returns value of type string
-func newCard() string {
-	return "Five of Clubs"
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
