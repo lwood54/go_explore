fd8240c3db26f68a3761cb8ca98f467b1b25065b
Logan Wood <lwood3499@gmail.com>
2020-11-23 11:20:32 -0600

    first commit

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Diamonds"
+	card := "Ace of Diamonds" // alternate way of declaring and defining variable and inferring type
+	card = "Five of Spades"   // reassigning card with same type
+
+	fmt.Println(card)
+}
