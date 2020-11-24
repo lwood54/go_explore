7cfe3e7c14c0c78fdb17c486c5807e044c6a3c6e
Logan Wood <lwood3499@gmail.com>
2020-11-24 15:38:37 -0600

    interfaces_assignment update

---
+++ interfaces_assignment/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	tr := triangle{base: 54, height: 25}
+	sq := square{sideLength: 34}
+
+	printArea(tr)
+	printArea(sq)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s, " has area of: ", s.getArea())
+}
