78296c6862a5ed272e63a796e67ebbe185c03b79
Logan Wood <lwood3499@gmail.com>
2020-11-24 12:25:01 -0600

    maps vs structs

--- maps/main.go
+++ maps/main.go
@@ -3,29 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	// option 1
-	// declaring a variable where the keys are of type string and the values are all of type string as well
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
 
-	// option 2
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// option 3
-	colors := make(map[string]string)
-	// add in key/value pairs after initialization
-	colors["white"] = "#ffffff" // cannot access with dot notation, this is because all keys are typed
 	fmt.Println(colors)
-
-	// option 3 with ints/strings
-	otherColors := make(map[int]string)
-	otherColors[10] = "#fff000"
-	fmt.Println(otherColors)
-
-	// how to delete a key/value pair from a map
-	delete(otherColors, 10)
-	fmt.Println(otherColors)
+	printMap(colors)
 
 }
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
+}
+
+//// MAPS  vs STRUCTS ////
+//					Map															//														Struct						//
+//	1. All values must be the same type			//	1. Values can be of different type
+//	2. Use to represent a collection				//	2. Used to represent a 'thing' with
+//		of related properties													a lot of different properties
+//	3. Keys are indexed - we can iterate		//	3. Keys don't support indexing
+//			over them
+//	4. Don't need to know all the keys			//	4. You need to know all the different fields at compile time
+//			at compile time
+//	5. Reference Type												//	5. Value Type
+//	6. All keys must be the same type
